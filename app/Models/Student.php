70f09c59c3205545765042b507b8e6eb7d3f5f6c
atriexz4pato <kungapatrick81@gmail.com>
2025-01-16 08:25:47 +0300

    add student model

---
+++ app/Models/Student.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    //
+    protected $fillable = [
+        'programme',
+        'attachment_county',
+
+        'address',
+
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function assessor(){
+        return $this->belongsToMany(Assessor::class);
+    }
+
+    public function assessment(){
+        return $this->hasMany(Assessment::class);
+    }
+}
