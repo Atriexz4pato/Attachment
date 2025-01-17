21d7d97a5d806e455d7712ad9491662f13a658cb
atriexz4pato <kungapatrick81@gmail.com>
2025-01-17 20:46:49 +0300

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
+        'department',
         'password',
     ];
 
@@ -44,5 +45,13 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function student(){
+        return $this->hasOne(Student::class);
+    }
+
+    public function assessor(){
+        return $this->hasOne(Assessor::class);
     }
 }
